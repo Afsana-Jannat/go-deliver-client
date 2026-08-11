c76c7f134771ea4018c61e3e0586fca96e0589cb
Afsana-Jannat <afsanajannat205@gmail.com>
2026-08-11 17:34:48 +0600

    vite include

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,39 +1,91 @@
+// import axios from "axios";
+// import useAuth from "./useAuth";
+// import { useNavigate } from "react-router";
+
+// const axiosSecure = axios.create({
+//     baseURL: `${import.meta.env.VITE_BASE_URL}/`
+// })
+
+// const useAxiosSecure = () => {
+//     const { user, logOut } = useAuth();
+//     // const { navigate } = useNavigate();
+//     const navigate = useNavigate();
+
+//     axiosSecure.interceptors.request.use(config => {
+//         config.headers.Authorization = `Bearer ${user.accessToken}`
+//         return config;
+//     }, error => {
+//         return Promise.reject(error);
+//     })
+
+//     axiosSecure.interceptors.response.use(res => {
+//         return res;
+//     }, error => {
+//         const status = error.status;
+//         if (status === 403) {
+//             navigate('/forbidden')
+//         }
+//         else if (status === 401) {
+//             logOut()
+//                 .then(() => {
+//                     navigate('/login')
+//                 })
+//                 .catch(() => { })
+//         }
+
+//         return Promise.reject(error)
+//     })
+
+//     return axiosSecure;
+// };
+
+// export default useAxiosSecure;
+
 import axios from "axios";
 import useAuth from "./useAuth";
 import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/`
-})
+    baseURL: `${import.meta.env.VITE_BASE_URL}/`,
+});
 
 const useAxiosSecure = () => {
     const { user, logOut } = useAuth();
-    const { navigate } = useNavigate();
+    const navigate = useNavigate();
 
-    axiosSecure.interceptors.request.use(config => {
-        config.headers.Authorization = `Bearer ${user.accessToken}`
-        return config;
-    }, error => {
-        return Promise.reject(error);
-    })
+    axiosSecure.interceptors.request.use(
+        (config) => {
+            if (user?.accessToken) {
+                config.headers.Authorization = `Bearer ${user.accessToken}`;
+            }
 
-    axiosSecure.interceptors.response.use(res => {
-        return res;
-    }, error => {
-        const status = error.status;
-        if (status === 403) {
-            navigate('/forbidden')
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
         }
-        else if (status === 401) {
-            logOut()
-                .then(() => {
-                    navigate('/login')
-                })
-                .catch(() => { })
-        }
+    );
 
-        return Promise.reject(error)
-    })
+    axiosSecure.interceptors.response.use(
+        (res) => {
+            return res;
+        },
+        (error) => {
+            const status = error.response?.status;
+
+            if (status === 403) {
+                navigate("/forbidden");
+            } else if (status === 401) {
+                logOut()
+                    .then(() => {
+                        navigate("/login");
+                    })
+                    .catch(() => {});
+            }
+
+            return Promise.reject(error);
+        }
+    );
 
     return axiosSecure;
 };
